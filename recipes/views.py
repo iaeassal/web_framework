a9fd9b062776f5e7382c5db46994f634b859ce00
iaeassal <iaeassal>
2024-09-24 12:22:07 -0300

    Render

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
 from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home (request):
     return render(request, 'recipes/home.html')
-
-def sobre (request):
-    return HttpResponse('Sobre')
-
-def contato (request):
-    return HttpResponse('Contato 1')
