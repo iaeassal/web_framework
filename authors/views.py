e96a8d636cac86bc46bc72fc8599a3ca5d0951e9
iaeassal <iaeassal>
2024-11-25 10:00:44 -0300

    Logout

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -14,8 +15,6 @@ def register_view(request):
         'form': form,
         'form_action': reverse('authors:register_create'),
     })
-
-
 def register_create(request):
     if not request.POST:
         raise Http404()
@@ -27,9 +26,11 @@ def register_create(request):
         user.set_password(user.password)
         user.save()
         messages.success(request, 'Your user is created, please log in.')
-        del(request.session['register_form_data'])
-    return redirect('authors:register')
 
+        del(request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
+
+    return redirect('authors:register')
 
 def login_view(request):
     form = LoginForm()
@@ -37,21 +38,16 @@ def login_view(request):
         'form': form,
         'form_action': reverse('authors:login_create')
     })
-
-
 def login_create(request):
     if not request.POST:
         raise Http404()
-
     form = LoginForm(request.POST)
     login_url = reverse('authors:login')
-
     if form.is_valid():
         authenticated_user = authenticate(
             username=form.cleaned_data.get('username', ''),
             password=form.cleaned_data.get('password', ''),
         )
-
         if authenticated_user is not None:
             messages.success(request, 'Your are logged in.')
             login(request, authenticated_user)
@@ -61,3 +57,15 @@ def login_create(request):
         messages.error(request, 'Invalid username or password')
 
     return redirect(login_url)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+
+    logout(request)
+    return redirect(reverse('authors:login'))
